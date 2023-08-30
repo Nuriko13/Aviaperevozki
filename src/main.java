f35f30fede5c2faab83f1cf166b65dd12ed70312
Neto test <nurlan264@mail.ru>
2023-08-30 20:48:12 +0300

    Pervaya zadaca

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+
+    public static void main(String[] args) {
+        int price = 13676;
+
+        int bonus = price / 20;
+        System.out.println("бонус " + bonus);
+    }
+}
